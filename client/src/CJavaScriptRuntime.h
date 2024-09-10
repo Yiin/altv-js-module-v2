4d98df55956c7d3a47dd3a98c289f2d425feca4a
xLuxy <67131061+xLuxy@users.noreply.github.com>
2024-09-10 12:27:16 +0200

    fix(client): Fix OnHeapOOM

--- client/src/CJavaScriptRuntime.h
+++ client/src/CJavaScriptRuntime.h
@@ -10,7 +10,7 @@ class CJavaScriptRuntime : public js::IRuntime<CJavaScriptRuntime, CJavaScriptRe
     std::unique_ptr<v8::Platform> platform;
 
     static void OnFatalError(const char* location, const char* message);
-    static void OnHeapOOM(const char* location, bool isHeap);
+    static void OnHeapOOM(const char* location, const v8::OOMDetails& details);
     static size_t OnNearHeapLimit(void*, size_t current, size_t initial);
     static void OnPromiseRejected(v8::PromiseRejectMessage message);
     static v8::MaybeLocal<v8::Promise> ImportModuleDynamically(
