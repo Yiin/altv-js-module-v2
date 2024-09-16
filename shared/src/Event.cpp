0aeec1077e9e3bb6f5c71fa03b310fcb548a15f0
xLuxy <67131061+xLuxy@users.noreply.github.com>
2024-09-16 11:34:10 +0200

    fix(shared): Fix RESOURCE_STOP freezing

--- shared/src/Event.cpp
+++ shared/src/Event.cpp
@@ -27,21 +27,13 @@ void js::Event::SendEvent(const alt::CEvent* ev, IResource* resource)
 
     eventHandler->argsCb(ev, eventArgs);
 
-    js::Promise promise = CallEventBinding(false, (int)ev->GetType(), eventArgs, resource);
+    CallEventBinding(false, (int)ev->GetType(), eventArgs, resource);
     eventArgs.Get()->SetAlignedPointerInInternalField(1, nullptr);
-
-    if (!promise.IsValid()) return;
-
-    if (ev->GetType() == alt::CEvent::Type::RESOURCE_STOP && static_cast<const alt::CResourceStopEvent*>(ev)->GetResource() == resource->GetResource())
-    {
-        promise.Await();
-    }
 }
 
 void js::Event::SendEvent(EventType type, EventArgs& args, IResource* resource)
 {
-    js::Promise promise = CallEventBinding(true, (int)type, args, resource);
-    if(!promise.IsValid()) return;
+    CallEventBinding(true, (int)type, args, resource);
 }
 
 // Class
